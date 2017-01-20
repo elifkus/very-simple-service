955354fbe1bfa15432c0d460bf74645b4a214018
Elif T. Kuş <elifkus@gmail.com>
2017-01-20 12:06:47 +0200

    Change owner of config file to nginx

--- scripts/update_conf.sh
+++ scripts/update_conf.sh
@@ -3,4 +3,6 @@ NGINX_CONF_DIR=/etc/nginx/conf.d
 cd $WORKSPACE
 cp -rf $WORKSPACE/conf/experiment.conf $NGINX_CONF_DIR/.
 
+chown nginx.nginx $NGINX_CONF_DIR/experiment.conf
+
 /usr/sbin/service nginx reload
