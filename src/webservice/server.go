6b98a7793d78c8ca51293400263e17da6936580e
Elif T. Kuş <elifkus@gmail.com>
2017-01-20 14:42:30 +0200

    Return error if file already exists

--- src/webservice/server.go
+++ src/webservice/server.go
@@ -15,6 +15,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
     if _, err := os.Stat(path); err != nil {
 	    if os.IsNotExist(err) {
 		    http.Error(w, err.Error(), http.StatusInternalServerError)
+		    return
 	    }
     }
     
