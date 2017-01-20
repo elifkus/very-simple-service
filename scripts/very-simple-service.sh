bed218602352c386140306e2e2296c94b4928680
Elif T. Kuş <elifkus@gmail.com>
2017-01-20 14:07:39 +0200

    Added startup script

---
+++ scripts/very-simple-service.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+case $1 in
+    start)
+        echo "Starting very simple web service."
+        /PRODUCTION/EXPERIMENT/web/gobin/webservice &
+        ;;
+    stop)
+        echo "Stopping very simple web service."
+        sudo kill $(sudo lsof -t -i:8080)
+        ;;
+    *)
+        echo "Very simple web service."
+        echo $"Usage $0 {start|stop}"
+        exit 1
+esac
+exit 0
