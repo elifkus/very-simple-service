a8188834c230712818525f9a2048e1c059930353
Elif T. Kuş <elifkus@gmail.com>
2017-01-20 14:21:59 +0200

    Added restart to start script

--- scripts/very-simple-service.sh
+++ scripts/very-simple-service.sh
@@ -8,9 +8,17 @@ case $1 in
         echo "Stopping very simple web service."
         sudo kill $(sudo lsof -t -i:8080)
         ;;
+    restart)
+        echo "Restarting very simple web service."
+        
+        if [ $(lsof -t -i :8080) ]; then
+		   sudo kill -9 $(lsof -t -i :8080)
+		fi
+        /PRODUCTION/EXPERIMENT/web/gobin/webservice &
+        ;;
     *)
         echo "Very simple web service."
-        echo $"Usage $0 {start|stop}"
+        echo $"Usage $0 {start|stop|restart}"
         exit 1
 esac
 exit 0
