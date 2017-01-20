4d9f24abcbcdc860cfd1cfa6a2891fd3b56296bd
Elif T. Kuş <elifkus@gmail.com>
2017-01-20 12:12:20 +0200

    Created folder for log file in script

--- scripts/update_conf.sh
+++ scripts/update_conf.sh
@@ -5,4 +5,7 @@ cp -rf $WORKSPACE/conf/experiment.conf $NGINX_CONF_DIR/.
 
 chown nginx.nginx $NGINX_CONF_DIR/experiment.conf
 
+mkdir -p /PRODUCTION/EXPERIMENT/log/
+chown nginx.nginx /PRODUCTION/EXPERIMENT/log/
+
 /usr/sbin/service nginx reload
