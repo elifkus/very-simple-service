772da799b19a4d29bc7af139bc9dc5b488f95e08
Elif T. Kuş <elifkus@gmail.com>
2017-01-20 13:49:55 +0200

    If file exists the service will return 5xx

--- src/webservice/server.go
+++ src/webservice/server.go
@@ -3,19 +3,28 @@ package main
 import (
     "net/http"
     "io/ioutil"
+    "os"
 )
 
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
 	folder := "/PRODUCTION/EXPERIMENT/savedfiles/"
-    path := folder + generateRandomURL()
+    filename := generateRandomURL()
+    path := folder + filename
+    
+    if _, err := os.Stat(path); err != nil {
+	    if os.IsNotExist(err) {
+		    http.Error(w, err.Error(), http.StatusInternalServerError)
+	    }
+    }
+    
     text := r.FormValue("text")
-	ioutil.WriteFile(path, text, 0400)
+	ioutil.WriteFile(path, []byte(text), 0400)
 	
-    http.Redirect(w, r, "/"+filename, http.StatusCreated)
+	http.Redirect(w, r, "/"+filename, http.StatusCreated)
 }
 
-function generateRandomURL() string {
+func generateRandomURL() string {
 	return "1234556"
 }
 
