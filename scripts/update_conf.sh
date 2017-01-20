edff4bfbf1b72183561748bac01cbee92a4b697c
Elif T. Kuş <elifkus@gmail.com>
2017-01-20 12:01:47 +0200

    Fixed folder variable in update_conf.sh

--- scripts/update_conf.sh
+++ scripts/update_conf.sh
@@ -1,6 +1,6 @@
 NGINX_CONF_DIR=/etc/nginx/conf.d
 
 cd $WORKSPACE
-cp -rf $WORKSPACE/conf/experiment.conf $NGINX_CONF/.
+cp -rf $WORKSPACE/conf/experiment.conf $NGINX_CONF_DIR/.
 
 /usr/sbin/service nginx reload
