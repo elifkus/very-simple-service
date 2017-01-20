004e6cef7e6c8084a306d92d4b912b7f153a4380
Elif T. Kuş <elifkus@gmail.com>
2017-01-20 13:26:57 +0200

    Changed name of main.go to server.go
    Added deployment of go binary to deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,27 @@ GOPATH=$WORKSPACE
 
 mkdir -p $PROJECT_DIR/static
 mkdir -p $PROJECT_DIR/savedfiles
+mkdir -p $PROJECT_DIR/gobin
+
+cd $WORKSPACE
+cp -rf $WORKSPACE/src/* ./src/.
+#this gets the dependencies but doesn't install them, avoiding permission problems
+/usr/local/go/bin/go get -d
+#build the packages, -x outputs the compiler command line
+/usr/local/go/bin/go build -x
+mv server $PROJECT_DIR/gobin
+chmod +x $PROJECT_DIR/gobin/server
 
 cd $PROJECT_DIR
 cp -rf $WORKSPACE/static/* ./static/.
-
 chown -R nginx.nginx static
+
+
+if [ $(lsof -t -i :8080) ]; then
+   kill -9 $(lsof -t -i :8080)
+fi
+
+BUILD_ID=dontKillMe nohup ./server &>/dev/null &
+
+
 
