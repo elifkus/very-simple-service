29b72b660f23518ed08a2c9adb1136049bb71f8f
Elif T. Kuş <elifkus@gmail.com>
2017-02-01 17:11:29 +0200

    Added LUA_PATH and LUA_CPATH to benchmark script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -9,5 +9,10 @@ cp -rf data $PROJECT_DIR/.
 
 cd $PROJECT_DIR
 
-source /root/.bash_profile
+export LUA_PATH="/usr/local/lua/5.1/?.lua;./modules/?.lua;/usr/local/lua/5.1/?/?.lua;
+                /usr/share/lua/5.1;/usr/share/lua/5.1/?.lua;/usr/share/lua/5.1/?/?.lua;;"
+
+export LUA_CPATH="./?.so;/usr/local/lib/lua/5.1/?.so;/usr/local/lua/5.1/?.so;/usr/share/lua/5.1/?.so;/usr/lib64/lua/5.1/?.so;
+                /usr/share/lua/5.1/?.so;;"
+
 /usr/local/bin/wrk -c1 -t1 -d5s -s scripts/requests-with-post-data.lua http://experiment.safkanyazilim.com/
